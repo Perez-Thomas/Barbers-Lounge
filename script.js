ad229d53df953abeabbc99d64432e94ad20bcbde
Thomas Perez <thomas.v.perez@gmail.com>
2022-08-17 23:13:24 -0400

    The Barbers Lounge update

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 // Intro Pop Up On Page
-let pageIntro = 'pageIntro';
-if (pageIntro === 'pageIntro') alert('Welcome To The Shop');
+let pageIntro = "pageIntro";
+// if (pageIntro === "pageIntro") alert("Welcome To The Shop");
 
 //Slideshow
 
-
-
 // threeCards Buttons
 // FOR THESE THREE I HAVE TO DOUBLE CLICK.....WHY?
-document.getElementById("adamButton").onclick = function(){
+document.getElementById("adamButton").onclick = function () {
   document.getElementById("myDIV").style.display = "block";
   preventDefault();
 };
-document.getElementById("adamSubmit").onclick = function(){
+document.getElementById("adamSubmit").onclick = function () {
   document.getElementById("myDIV").style.display = "none";
   event.preventDefault();
 };
 
-
-document.getElementById("danielButton").onclick = function(){
+document.getElementById("danielButton").onclick = function () {
   document.getElementById("myDIV1").style.display = "block";
   preventDefault();
 };
-document.getElementById("danielSubmit").onclick = function(){
+document.getElementById("danielSubmit").onclick = function () {
   document.getElementById("myDIV1").style.display = "none";
   event.preventDefault();
 };
 
-
-document.getElementById("abrahamButton").onclick = function(){
+document.getElementById("abrahamButton").onclick = function () {
   document.getElementById("myDIV2").style.display = "block";
   preventDefault();
 };
-document.getElementById("abrahamSubmit").onclick = function(){
+document.getElementById("abrahamSubmit").onclick = function () {
   document.getElementById("myDIV2").style.display = "none";
   event.preventDefault();
 };
-
 
 // function myFunction() {
 //   var x = document.getElementById("myDIV");
@@ -47,27 +42,26 @@ document.getElementById("abrahamSubmit").onclick = function(){
 //   }
 // };
 
-
 // ---------------------
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-}
+};
 
+// REVEAL
